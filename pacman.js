ad41210a8d48cae44af6e2e3f2f2f4204c491c8f
Vikil Naik <vikilnaik37@gmail.com>
2019-03-22 16:08:01 -0400

    added ghosts as variables

--- pacman.js
+++ pacman.js
@@ -5,59 +5,89 @@ let lives = 2;
 
 // Define your ghosts here
 
-// replace this comment with your four ghosts setup as objects
+const inky = {
+    menu_option: '1',
+    name: 'Inky',
+    colour: 'Red',
+    character: 'Shadow',
+    edible: false
+};
+
+const blinky = {
+    menu_option: '2',
+    name: 'Blinky',
+    colour: 'Cyan',
+    character: 'Speedy',
+    edible: false
+}
+
+const pinky = {
+    menu_option: '3',
+    name: 'Pinky',
+    colour: 'Pink',
+    character: 'Bashful',
+    edible: false
+}
+
+const clyde = {
+    menu_option: '4',
+    name: 'Clyde',
+    colour: 'Orange',
+    character: 'Pokey',
+    edible: false
+}
 
 
 // Draw the screen functionality
 function drawScreen() {
-  clearScreen();
-  setTimeout(() => {
-    displayStats();
-    displayMenu();
-    displayPrompt();
-  }, 10);
+    clearScreen();
+    setTimeout(() => {
+        displayStats();
+        displayMenu();
+        displayPrompt();
+    }, 10);
 }
 
 function clearScreen() {
-  console.log('\x1Bc');
+    console.log('\x1Bc');
 }
 
 function displayStats() {
-  console.log(`Score: ${score}     Lives: ${lives}`);
+    console.log(`Score: ${score}     Lives: ${lives}`);
 }
 
 function displayMenu() {
-  console.log('\n\nSelect Option:\n');  // each \n creates a new line
-  console.log('(d) Eat Dot');
-  console.log('(q) Quit');
+    console.log('\n\nSelect Option:\n'); // each \n creates a new line
+    console.log('(d) Eat Dot');
+    console.log('(q) Quit');
 }
 
 function displayPrompt() {
-  // process.stdout.write is similar to console.log except it doesn't add a new line after the text
-  process.stdout.write('\nWaka Waka :v '); // :v is the Pac-Man emoji.
+    // process.stdout.write is similar to console.log except it doesn't add a new line after the text
+    process.stdout.write('\nWaka Waka :v '); // :v is the Pac-Man emoji.
 }
 
 
 // Menu Options
 function eatDot() {
-  console.log('\nChomp!');
-  score += 10;
+    console.log('\nChomp!');
+    score += 10;
 }
 
 
 // Process Player's Input
 function processInput(key) {
-  switch(key) {
-    case '\u0003': // This makes it so CTRL-C will quit the program
-    case 'q':
-      process.exit();
-      break;
-    case 'd':
-      eatDot();
-      break;
-    default:
-      console.log('\nInvalid Command!');
-  }
+    switch (key) {
+        case '\u0003': // This makes it so CTRL-C will quit the program
+        case 'q':
+            process.exit();
+            break;
+        case 'd':
+            eatDot();
+            break;
+        default:
+            console.log('\nInvalid Command!');
+    }
 }
 
 
@@ -76,12 +106,12 @@ drawScreen();
 
 // Process input and draw screen each time player enters a key
 stdin.on('data', (key) => {
-  process.stdout.write(key);
-  processInput(key);
-  setTimeout(drawScreen, 300); // The command prompt will flash a message for 300 milliseoncds before it re-draws the screen. You can adjust the 300 number to increase this.
+    process.stdout.write(key);
+    processInput(key);
+    setTimeout(drawScreen, 300); // The command prompt will flash a message for 300 milliseoncds before it re-draws the screen. You can adjust the 300 number to increase this.
 });
 
 // Player Quits
 process.on('exit', () => {
-  console.log('\n\nGame Over!\n');
+    console.log('\n\nGame Over!\n');
 });
